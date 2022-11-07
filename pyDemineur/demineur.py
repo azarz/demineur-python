8cbbbc37ba1bb524ee39b3ff9234c3e416884a7f
Amaury Zarzelli <amaury.zarzelli@gmail.com>
2022-11-07 16:08:25 +0100

    Ajout du mode console en option via argparse

--- pyDemineur/demineur.py
+++ pyDemineur/demineur.py
@@ -1,16 +1,29 @@
-import sys
+import argparse
 import os
+import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from PyQt5.QtWidgets import QApplication
 from pyDemineur.ui.gui import DemineurFenetre
+from pyDemineur.core.partie import Partie
 
 if __name__ == "__main__":
-    app = QApplication.instance()
-    if not app:
-        app = QApplication(sys.argv)
+    parser = argparse.ArgumentParser(prog = 'pyDemineur',
+                    description = 'Démineur écrit en Python')
+    parser.add_argument('-c', '--console', action='store_true')
+    args = parser.parse_args()
 
-    fen = DemineurFenetre()
-    fen.show()
+    if args.console:
+        print("Lancement du démineur en mode console")
+        partie = Partie(10, [8, 8])
+        partie.jouer()
+    else:
+        print("Lancement du démineur en mode fenêtre")
+        app = QApplication.instance()
+        if not app:
+            app = QApplication(sys.argv)
 
-    app.exec_()
+        fen = DemineurFenetre()
+        fen.show()
+
+        app.exec_()
